8540bb6061bb15a0910d7a93a640c96056429aca
veerr <rein.vanveer@kadaster.nl>
2017-01-25 16:50:10 +0100

    Dummy test setup

---
+++ src/test/indexTest.js
@@ -0,0 +1,16 @@
+/**
+ * Created by veerr on 25-1-2017.
+ */
+/* eslint-env mocha */
+
+'use strict';
+
+const chai = require('chai');
+
+chai.should();
+
+describe('index', () => {
+  it('executes a dummy test', () => {
+    true.should.equal(true);
+  });
+});
