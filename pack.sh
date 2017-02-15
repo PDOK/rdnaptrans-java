87315e541462590285a220e784f6f9ad42db4562
reinvantveer <rein.van.t.veer@geodan.nl>
2017-02-15 16:48:02 +0100

    Browser test fiddling

---
+++ pack.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/env bash
+node_modules/.bin/webpack --config webpack.test.config.js
--- test-all.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-bash -c "./node_modules/.bin/mocha --recursive -R spec --bail --check-leaks test/server &&
-./node_modules/.bin/karma start --browsers Firefox --single-run"
